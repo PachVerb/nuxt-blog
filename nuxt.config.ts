16db4d7805079ff035b1e5a7f2757df996b7fa53
wangshan <22212@qq.com>
2022-07-18 20:44:47 +0800

    chore: init engeneer

---
+++ nuxt.config.ts
@@ -0,0 +1,11 @@
+/*
+ * @LastEditTime: 2022-07-18 20:43:39
+ * @Description:
+ * @Date: 2022-07-14 02:20:57
+ * @Author: wangshan
+ * @LastEditors: wangshan
+ */
+import { defineNuxtConfig } from "nuxt";
+
+// https://v3.nuxtjs.org/api/configuration/nuxt.config
+export default defineNuxtConfig({});
