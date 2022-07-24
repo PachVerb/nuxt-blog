55be1f4f6dc355202f45588980d2d1135b680e9d
wangshan <22212@qq.com>
2022-07-24 18:04:44 +0800

    docs(build): 添加构建选项说明
    - WebComponent
    - Vue Component

---
+++ components/Tab/TableItem.vue
@@ -0,0 +1,11 @@
+<!--
+ * @LastEditTime: 2022-07-24 17:51:51
+ * @Description: 
+ * @Date: 2022-07-24 17:50:50
+ * @Author: wangshan
+ * @LastEditors: wangshan
+-->
+<script lang="ts">
+export default defineComponent({});
+</script>
+<template></template>
---
+++ components/Tab/index.vue
@@ -0,0 +1,7 @@
+<!--
+ * @LastEditTime: 2022-07-24 17:50:38
+ * @Description: 
+ * @Date: 2022-07-24 17:50:37
+ * @Author: wangshan
+ * @LastEditors: wangshan
+-->
